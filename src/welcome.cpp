599d3b67d2bf810d1312e8d7dd38fb73c01e4494
Yves Ledermann <yves@laedis.ch>
2017-02-06 21:42:33 +0100

    testing includes

---
+++ src/welcome.cpp
@@ -0,0 +1,27 @@
+// -----------------------------------------------------------------------------
+// Welcome MSG
+// -----------------------------------------------------------------------------
+
+void welcome() {
+
+    delay(2000);
+    Serial.printf("%s %s\n", (char *) APP_NAME, (char *) APP_VERSION);
+    Serial.printf("%s\n%s\n\n", (char *) APP_AUTHOR, (char *) APP_WEBSITE);
+    Serial.printf("Device: %s\n", (char *) WiFi.hostname().c_str());
+    Serial.printf("ChipID: %06X\n", ESP.getChipId());
+    Serial.printf("Last reset reason: %s\n", (char *) ESP.getResetReason().c_str());
+    Serial.printf("Memory size: %d bytes\n", ESP.getFlashChipSize());
+    Serial.printf("Free heap: %d bytes\n", ESP.getFreeHeap());
+    FSInfo fs_info;
+    if (SPIFFS.info(fs_info)) {
+        Serial.printf("File system total size: %d bytes\n", fs_info.totalBytes);
+        Serial.printf("            used size : %d bytes\n", fs_info.usedBytes);
+        Serial.printf("            block size: %d bytes\n", fs_info.blockSize);
+        Serial.printf("            page size : %d bytes\n", fs_info.pageSize);
+        Serial.printf("            max files : %d\n", fs_info.maxOpenFiles);
+        Serial.printf("            max length: %d\n", fs_info.maxPathLength);
+    }
+    Serial.println();
+    Serial.println();
+
+}
