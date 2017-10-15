32c3000beb2e62a1bc7dc7f4fd7b8a192bdd2cc4
Yves Ledermann <yves@laedis.ch>
2017-10-15 21:37:58 +0200

    include all nodes per default (not necessary to inc each one)

--- src/homie-esp8266-supplements.h
+++ src/homie-esp8266-supplements.h
@@ -4,9 +4,10 @@
 #include "Misc/ota.hpp"
 #include "Misc/logging.hpp"
 
-#include "Nodes/Node_ds18b20Multi.hpp"
-#include "Nodes/Node_UltrasonicSerial.hpp"
-#include "Nodes/Node_DigitalIn.hpp"
 #include "Nodes/Node_AnalogIn.hpp"
+#include "Nodes/Node_DigitalIn.hpp"
+#include "Nodes/Node_ds18b20Multi.hpp"
+#include "Nodes/Node_ModbusPowerMeter_Eastron.hpp"
 #include "Nodes/Node_PulseCounter.hpp"
 #include "Nodes/Node_SerialSolarWaterSensor.hpp"
+#include "Nodes/Node_UltrasonicSerial.hpp"
