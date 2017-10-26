5a9c42553d3a625e0dbf7ab5ccd9b5db879a52ce
Yves Ledermann <yves@laedis.ch>
2017-10-26 20:31:50 +0200

    added Botton and Relay Nodes of Lübbe -> THANK YOU!!!
    github.com/luebbe/homie-node-collection

--- README.md
+++ README.md
@@ -4,3 +4,8 @@ Nodes and additional Code
 PlatformIO build enviroment.
 
 *** need to write more text***
+
+
+external Code Sources & Credits:
+
+* Thank you Lübbe for you Repo https://github.com/luebbe/homie-node-collection
---
+++ src/Nodes/Relay.hpp
@@ -0,0 +1,37 @@
+/*
+ * RelayNode.hpp
+ * Homie Node for a Relay with optional status indicator LED
+ *
+ * Version: 1.0
+ * Author: Lübbe Onken (http://github.com/luebbe)
+ */
+
+#ifndef SRC_RELAYNODE_H_
+#define SRC_RELAYNODE_H_
+
+#include <HomieNode.hpp>
+
+#define DEFAULTPIN -1
+
+class RelayNode : public HomieNode {
+private:
+  const char *cCaption  = "• Relay:";
+  const char *cIndent   = "  ◦ ";
+
+  int _relayPin;
+  int _ledPin;
+
+  void printCaption();
+  void setLed(bool on);
+
+protected:
+  virtual void setup() override;
+  virtual bool handleInput(const String& property, const HomieRange& range, const String& value) override;
+
+public:
+  RelayNode(const char *name, const int relayPin = DEFAULTPIN, const int ledPin = DEFAULTPIN);
+  void setRelay(bool on);
+  void toggleRelay();
+};
+
+#endif
